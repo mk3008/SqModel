575bfe1acedfa00271ec82ad93b940a46dbe9b34
mk3008 <mk3008.develop@gmail.com>
2022-06-23 20:56:03 +0900

    #9 CTE can be described by anonymous method
    It is implemented by an extension method.

---
+++ src/SqModel/SelectQuery.With.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqModel;
+
+public static class SelectQueryWith
+{
+    public static CommonTableClause With(this SelectQuery source, Action<SelectQuery> action, string alias)
+    {
+        var q = new SelectQuery();
+        action(q);
+        return source.With.Add(q, alias);
+    }
+
+    public static CommonTableClause With(this SelectQuery source, SelectQuery commonQuery, string alias)
+    {
+        return source.With.Add(commonQuery, alias);
+    }
+}
