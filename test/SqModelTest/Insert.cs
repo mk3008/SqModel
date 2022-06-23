8bfa7ec65538fe151ea1dd6fe885e120abffdae7
mk3008 <mk3008.develop@gmail.com>
2022-06-24 07:41:19 +0900

    #24 Supports insert queries
    Adds to the column name specified in the select query. If you use wildcards, the column names you add are omitted.

---
+++ test/SqModelTest/Insert.cs
@@ -0,0 +1,63 @@
+﻿using SqModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SqModelTest;
+
+public class Insert
+{
+    [Fact]
+    public void Default()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a","a");
+        q.Select(table_a, "id");
+
+        var tq = new InsertQuery() { SelectQuery = q, TableName = "table_b" };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"insert into table_b(id)
+select a.id
+from table_a as a";
+
+        Assert.Equal(expect, text);
+    }
+
+    [Fact]
+    public void ColumnNameOmitted()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a", "a");
+        q.Select(table_a, "*");
+
+        var tq = new InsertQuery() { SelectQuery = q, TableName = "table_b" };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"insert into table_b
+select a.*
+from table_a as a";
+
+        Assert.Equal(expect, text);
+    }
+
+    [Fact]
+    public void ColumnNameAlias()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a", "a");
+        q.Select(table_a, "id", "index_value");
+
+        var tq = new InsertQuery() { SelectQuery = q, TableName = "table_b" };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"insert into table_b(index_value)
+select a.id as index_value
+from table_a as a";
+
+        Assert.Equal(expect, text);
+    }
+}
