2083836b61e70ecba52b42ae73331f84b346d3e3
mk3008 <mk3008.develop@gmail.com>
2022-06-23 21:13:00 +0900

    #11 Supports Create table
    Implement the selection by wrapping the query in the CreateTableQuery class.

---
+++ test/SqModelTest/CreateTable.cs
@@ -0,0 +1,48 @@
+﻿using SqModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SqModelTest;
+
+public class CreateTable
+{
+    [Fact]
+    public void Default()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a");
+        q.Select(table_a, "*");
+
+        var tq = new CreateTableQuery() { SelectQuery = q, TableName = "tmp" };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"create table tmp
+as
+select table_a.*
+from table_a";
+
+        Assert.Equal(expect, text);
+    }
+
+    [Fact]
+    public void Temporary()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a");
+        q.Select(table_a, "*");
+
+        var tq = new CreateTableQuery() { SelectQuery = q, TableName = "tmp", IsTemporary = true };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"create temporary table tmp
+as
+select table_a.*
+from table_a";
+
+        Assert.Equal(expect, text);
+    }
+}
