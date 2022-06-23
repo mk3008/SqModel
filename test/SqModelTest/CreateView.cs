eb1526439acaf63ae1cb836826ae7d5aab47319c
mk3008 <mk3008.develop@gmail.com>
2022-06-23 21:19:07 +0900

    #13 Supports Create view
    The CreateView class wraps and implements the select query.
    It has the same implementation method as CreateTable.

---
+++ test/SqModelTest/CreateView.cs
@@ -0,0 +1,48 @@
+﻿using SqModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SqModelTest;
+
+public class CreateView
+{
+    [Fact]
+    public void Default()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a");
+        q.Select(table_a, "*");
+
+        var tq = new CreateViewQuery() { SelectQuery = q, ViewName = "tmp" };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"create view tmp
+as
+select table_a.*
+from table_a";
+
+        Assert.Equal(expect, text);
+    }
+
+    [Fact]
+    public void Temporary()
+    {
+        var q = new SelectQuery();
+        var table_a = q.From("table_a");
+        q.Select(table_a, "*");
+
+        var tq = new CreateViewQuery() { SelectQuery = q, ViewName = "tmp", IsTemporary = true };
+
+        var text = tq.ToQuery().CommandText;
+        var expect = @"create temporary view tmp
+as
+select table_a.*
+from table_a";
+
+        Assert.Equal(expect, text);
+    }
+}
