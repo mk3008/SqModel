c17cda128f237e2d5117fdfbae7d6063be9b5f14
mk3008 <mk3008.develop@gmail.com>
2022-07-11 20:47:31 +0900

    #23 Implementation of TableParse

---
+++ src/SqModel/Serialization/Parser.Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqModel.Serialization;
+
+public partial class Parser
+{
+	public SelectTable ParseSelectTable()
+	{
+		// (schema_name.)table_name( as alias)
+
+		var tbl = new SelectTable();
+
+		ReadSkipSpaces();
+		var s = ReadUntil(" .");
+
+		var cn = PeekOrDefault();
+		if (cn == '.')
+		{
+			tbl.Schema = s;
+			Read();
+			tbl.TableName = ReadUntilSpace();
+			tbl.AliasName = ParseTableAliasOrDefault() ?? string.Empty;
+		}
+		else
+		{
+			tbl.TableName = s;
+			tbl.AliasName = ParseTableAliasOrDefault() ?? string.Empty;
+		}
+
+		return tbl;
+	}
+}
---
+++ src/SqModel/Serialization/SelectTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqModel.Serialization;
+
+public class SelectTable
+{
+    public string Schema { get; set; } = string.Empty;
+
+    public string TableName { get; set; } = string.Empty;
+
+    public string AliasName { get; set; } = string.Empty;
+}
