d007654304b51f2702d16aa966372bc0aebb5a01
mk3008 <mk3008.develop@gmail.com>
2022-11-01 21:55:52 +0900

    Added function "ToCommandText" to get the command part of SelectItem.

---
+++ test/SqModelTest/Element.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using SqModel;
+using SqModel.Analysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SqModelTest;
+
+public class Element
+{
+    public Element(ITestOutputHelper output)
+    {
+        Output = output;
+    }
+
+    private readonly ITestOutputHelper Output;
+
+    [Fact]
+    public void Default()
+    {
+        //using SqModel;
+        var sq = SqlParser.Parse("select a.id, a.id as col, 1+1 as val from a");
+
+        var a = sq.FromClause;
+        sq.Select.Add().Column(a, "column1");
+        sq.Select.Add().Column(a, "column2").As("column2");
+        sq.Select.Add().Column(a, "column3").As("col3");
+
+        sq.GetSelectItems().ForEach(x =>
+        {
+            Output.WriteLine($"> {x.ToQuery().CommandText}");
+            Output.WriteLine($"  name : {x.Name}");
+            Output.WriteLine($"  column name : {x.ColumnName}");
+            Output.WriteLine($"  command text : {x.ToCommandText()}");
+
+            var expect = string.Empty;
+            if (x.Command != null)
+            {
+                expect = x.Command.ToQuery().CommandText;
+            }
+            Assert.Equal(expect, x.ToCommandText());
+        });
+    }
+}
