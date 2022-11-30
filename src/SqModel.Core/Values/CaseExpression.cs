1ae1778b5c0f33ccf093b15f254a392e529f046d
mk3008 <mk3008.develop@gmail.com>
2022-12-01 08:24:25 +0900

    Implementation of the CaseExpression class

---
+++ src/SqModel.Core/Values/CaseExpression.cs
@@ -0,0 +1,40 @@
+﻿using Cysharp.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqModel.Core.Values;
+
+public class CaseExpression : ValueBase
+{
+    public CaseExpression()
+    {
+    }
+
+    public CaseExpression(ValueBase condition)
+    {
+        CaseCondition = condition;
+    }
+
+    public ValueBase? CaseCondition { get; init; }
+
+    public List<WhenExpression> WhenExpressions { get; init; } = new();
+
+    public ValueBase? ElseValue { get; set; }
+
+    public override string GetCurrentCommandText()
+    {
+        var sb = ZString.CreateStringBuilder();
+        sb.Append("case");
+        if (CaseCondition != null) sb.Append(" " + CaseCondition.GetCommandText());
+        foreach (var item in WhenExpressions)
+        {
+            sb.Append(" " + item.GetCommandText());
+        }
+        if (ElseValue != null) sb.Append(" else " + ElseValue.GetCommandText());
+        sb.Append(" end");
+        return sb.ToString();
+    }
+}
---
+++ src/SqModel.Core/Values/WhenExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqModel.Core.Values;
+
+public class WhenExpression
+{
+    public WhenExpression(ValueBase condition, ValueBase value)
+    {
+        Condition = condition;
+        Value = value;
+    }
+
+    public ValueBase Condition { get; init; }
+
+    public ValueBase Value { get; init; }
+
+    public string GetCommandText()
+    {
+        return "when " + Condition.GetCommandText() + " then " + Value.GetCommandText();
+    }
+}
